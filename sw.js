b921e9f06aaa86f330eda630cb9c235afa2cc7aa
gooddeeds0204-eng <gooddeeds0204@gmail.com>
2026-09-24 02:25:32 +0530

    Update cached logo asset

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="yougo-ui-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./assets/yougo-logo.png"];
+const CACHE="yougo-ui-v2";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./assets/yougo-logo.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
