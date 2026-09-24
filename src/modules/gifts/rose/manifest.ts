6185f37b859c69b5c2ea3ba76e18d530c8c841fd
gooddeeds0204-eng <gooddeeds0204@gmail.com>
2026-09-24 13:11:47 +0530

    Register starter gift module

---
+++ src/modules/gifts/rose/manifest.ts
@@ -0,0 +1,11 @@
+import type { GiftManifest } from "@/contracts/gift";
+
+export const roseGiftManifest: GiftManifest = {
+  id: "rose",
+  name: "Rose",
+  icon: "🌹",
+  category: "popular",
+  diamondCost: 10,
+  enabled: true,
+  animationKey: "rose-basic",
+};
