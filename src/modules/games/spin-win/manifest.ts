59ed0c15b176c23a5edbd957cbe0c07f374e979d
gooddeeds0204-eng <gooddeeds0204@gmail.com>
2026-09-24 13:11:40 +0530

    Register Spin and Win module

---
+++ src/modules/games/spin-win/manifest.ts
@@ -0,0 +1,9 @@
+import type { GameManifest } from "@/contracts/game";
+
+export const spinWinManifest: GameManifest = {
+  id: "spin-win",
+  name: "Spin & Win",
+  icon: "🎡",
+  enabled: true,
+  supportsGamePk: false,
+};
