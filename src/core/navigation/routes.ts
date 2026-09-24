a30c478d4a9827d90424b10a1b628d63fb67185c
gooddeeds0204-eng <gooddeeds0204@gmail.com>
2026-09-24 13:08:47 +0530

    Add route constants

---
+++ src/core/navigation/routes.ts
@@ -0,0 +1,10 @@
+export const routes = {
+  welcome: "/welcome",
+  login: "/login",
+  otp: "/otp",
+  profileSetup: "/profile-setup",
+  home: "/",
+  discover: "/discover",
+  messages: "/messages",
+  profile: "/profile",
+} as const;
