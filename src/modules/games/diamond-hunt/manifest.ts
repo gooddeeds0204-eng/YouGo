134001e0d082a24eaecb0e11aa2da6f0d426dc9e
gooddeeds0204-eng <gooddeeds0204@gmail.com>
2026-09-24 13:11:43 +0530

    Register Diamond Hunt module

---
+++ src/modules/games/diamond-hunt/manifest.ts
@@ -0,0 +1,9 @@
+import type { GameManifest } from "@/contracts/game";
+
+export const diamondHuntManifest: GameManifest = {
+  id: "diamond-hunt",
+  name: "Diamond Hunt",
+  icon: "💎",
+  enabled: true,
+  supportsGamePk: true,
+};
